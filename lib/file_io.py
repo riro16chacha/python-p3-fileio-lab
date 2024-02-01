72e55aa884dfd6512597753111112d8a3ba98e2b
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2024-02-01 13:26:50 +0300

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    try:
+        with open(f"{file_name}.txt", "r") as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return "File not found."
